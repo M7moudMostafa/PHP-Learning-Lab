af553fab73b26083d3e39a1b613d3a7f5eb7e8f1
Mahmoud Mostafa <156802364+M7moudMostafa@users.noreply.github.com>
2025-07-06 10:55:07 +0300

    Assignments from 1 - 5 lessons

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+/*
+========================================================
+  File Name: index.php
+  Description: This is a simple PHP page that demonstrates:
+    - Multi-line comments
+    - UTF-8 encoding header
+    - Short echo tag usage
+    - Basic output using PHP inside HTML
+  Author: Mahmoud Mostafa
+  Date: 2025-07-06
+========================================================
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="<?php echo 'UTF-8'; ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "My First PHP Page" ?></title>
+  </head>
+  <body>
+    <div><?= "We Love" ?></div>
+    <div><?= "Elzero Channel" ?></div>
+  </body>
+</html>
+
+<?php
+/*
+========================================================
+  File Name: index.php
+  Description:
+	Use comments on each of the following lines to prevent the code from running.
+	You must use a different type of comment for each line.
+  Author: Mahmoud Mostafa
+  Date: 2025-07-06
+========================================================
+*/
+
+// echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please'; */
+# echo 'Prevent Me From Running Please';
+?>
+
+<?php
+/*
+========================================================
+  File Name: index.php
+  Description:
+	Correct the following comment to make it appropriate.
+		// My Application
+		// Version 1.0
+		// Created By Elzero
+  Author: Mahmoud Mostafa
+  Date: 2025-07-06
+========================================================
+*/
+
+// Fix code 
+
+/*
+	My Application
+	Version 1.0
+	Created By Elzero
+*/
+?>
+
+<?php
+/*
+========================================================
+  File Name: index.php
+  Description:
+	Which of the following comments are correct and which are incorrect?
+  Author: Mahmoud Mostafa
+  Date: 2025-07-06
+========================================================
+*/
+
+	// ## First Comment      => Cprrect
+	# // # iohifdjggh        => Correct
+	/* /* /* Third Comment */  //=> InCorrect
+	////// Fourth Comment    => Correct
+?>
